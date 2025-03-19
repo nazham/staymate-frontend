5ec0b0426cce98fe67e517a73f9947712d50c7ab
nazham <nazhammohamed@gmail.com>
2025-03-19 20:02:40 +0530

    first commit

---
+++ src/components/HotelListings.jsx
@@ -0,0 +1,118 @@
+import { useGetHotelsQuery } from "@/lib/api";
+import { useState } from "react";
+import HotelCard from "./HotelCard";
+import LocationTab from "./LocationTab";
+
+export default function HotelListings() {
+  const { data: hotels, isLoading, isError, error } = useGetHotelsQuery();
+
+  const locations = ["ALL", "France", "Italy", "Australia", "Japan"];
+
+  const [selectedLocation, setSelectedLocation] = useState("ALL");
+
+  const handleSelectedLocation = (location) => {
+    setSelectedLocation(location);
+  };
+
+  const filteredHotels =
+    selectedLocation === "ALL"
+      ? hotels
+      : hotels.filter((hotel) => {
+          return hotel.location
+            .toLowerCase()
+            .includes(selectedLocation.toLowerCase());
+        });
+
+  if (isLoading) {
+    return (
+      <section className="px-8 py-8 lg:py-16">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {locations.map((location, i) => {
+            return (
+              <LocationTab
+                key={i}
+                selectedLocation={selectedLocation}
+                name={location}
+                onClick={handleSelectedLocation}
+              />
+            );
+          })}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+          <p>Loading...</p>
+        </div>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className="px-8 py-8 lg:py-16">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {locations.map((location, i) => {
+            return (
+              <LocationTab
+                key={i}
+                selectedLocation={selectedLocation}
+                name={location}
+                onClick={handleSelectedLocation}
+              />
+            );
+          })}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+          <p className="text-red-500">{error}</p>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="px-8 py-8 lg:py-16">
+      <div className="mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4">
+          Top trending hotels worldwide
+        </h2>
+        <p className="text-lg text-muted-foreground">
+          Discover the most trending hotels worldwide for an unforgettable
+          experience.
+        </p>
+      </div>
+      <div className="flex items-center gap-x-4">
+        {locations.map((location, i) => {
+          return (
+            <LocationTab
+              key={i}
+              selectedLocation={selectedLocation}
+              name={location}
+              onClick={handleSelectedLocation}
+            />
+          );
+        })}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+        {filteredHotels.map((hotel) => {
+          return <HotelCard key={hotel._id} hotel={hotel} />;
+        })}
+      </div>
+    </section>
+  );
+}
---
+++ src/lib/api.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const BACKEND_URL = "http://localhost:8000";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${BACKEND_URL}/api/`,
+    prepareHeaders: async (headers, { getState }) => {
+      const token = await window?.Clerk?.session?.getToken();
+      console.log(token);
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+    }
+  }),
+  endpoints: (builder) => ({
+    getHotels: builder.query({
+      query: () => "hotels",
+    }),
+    getHotelById: builder.query({
+      query: (id) => `hotels/${id}`,
+    }),
+    createHotel: builder.mutation({
+      query: (hotel) => ({
+        url: "hotels",
+        method: "POST",
+        body: hotel,
+      }),
+    }),
+    createBooking: builder.mutation({
+      query: (booking) => ({
+        url: "bookings",
+        method: "POST",
+        body: booking,
+      }),
+    }),
+  }),
+});
+
+export const { useGetHotelsQuery, useGetHotelByIdQuery, useCreateHotelMutation, useCreateBookingMutation } =
+  api;
